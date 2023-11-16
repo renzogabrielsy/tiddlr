cff6992710bb3e52c99a52bfa6c4a629bfbe5358
Renzo Sy <renzogabrielsy@gmail.com>
2023-11-16 11:00:00 +0800

    Initial Commit

---
+++ tiddlr/tiddlrApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiddlrApp.swift
+//  tiddlr
+//
+//  Created by Renzo Sy on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct tiddlrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
